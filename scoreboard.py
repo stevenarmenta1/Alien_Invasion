979650668b4a1b15adce52dd0f7df44b99efe537
Steven Armenta <armentaa.steven@gmail.com>
2025-09-16 16:02:52 -0700

    Added Ship cound to the game

--- scoreboard.py
+++ scoreboard.py
@@ -1,10 +1,14 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 class Scoreboard:
     '''A class to report scoring information.'''
 
     def __init__(self, ai_game):
         '''Initialize scorekeeping attributes.'''
+        self.ai_game = ai_game
         self.screen = ai_game.screen
         self.screen_rect = self.screen.get_rect()
         self.settings = ai_game.settings
@@ -18,12 +22,12 @@ class Scoreboard:
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
-
+        self.prep_ships()
     
     def prep_score(self):
         '''Turn the score into a rendered image.'''
         rounded_score = round(self.stats.score, -1)
-        score_str = f"SCORE: {rounded_score:,}"
+        score_str = f"Score: {rounded_score:,}"
         self.score_image = self.font.render(score_str, True, 
                 self.text_color, self.settings.bg_color)
         
@@ -36,7 +40,7 @@ class Scoreboard:
     def prep_high_score(self):
         '''Turn the high score into a rendered image.'''
         high_score = round(self.stats.high_score, -1)
-        high_score_str = f"HIGH SCORE: {high_score:,}"
+        high_score_str = f"All-Time High Score: {high_score:,}"
         self.high_score_image = self.font.render(high_score_str, True, 
                         self.text_color, self.settings.bg_color)
         
@@ -48,7 +52,7 @@ class Scoreboard:
 
     def prep_level(self):
         '''Turn the level into a rendered image.'''
-        level_str = f"LVL: {self.stats.level}"
+        level_str = f"Lvl: {self.stats.level}"
         self.level_image = self.font.render(level_str, True,
                         self.text_color, self.settings.bg_color)
         
@@ -57,12 +61,23 @@ class Scoreboard:
         self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
 
+    
+    def prep_ships(self):
+        '''Show how many ships are left.'''
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_game)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)   
+
 
     def show_score(self):
-        '''Draw scores and level to the screen.'''
+        '''Draw scores, levels, and ships to the screen.'''
         self.screen.blit(self.score_image, self.score_rect)
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
+        self.ships.draw(self.screen)
     
     def check_high_score(self):
         '''Check to see if there's a new high score.'''
--- settings.py
+++ settings.py
@@ -21,12 +21,12 @@ class Settings:
 
         #alien settings
         self.alien_speed = 1.0
-        self.fleet_drop_speed = 15
+        self.fleet_drop_speed = 50
         # fleet_direction of 1 represents right, -1 represents left. 
         # self.fleet_direction = 1
         
         # How quickly the game speeds up
-        self.speedup_scale = 1.15
+        self.speedup_scale = 1.5
 
         # How quickly the alien point values increase. 
         self.score_scale = 1.5
@@ -36,7 +36,7 @@ class Settings:
 
     def initialize_dynamic_settings(self,):
         '''Initialize settings that change throughout the game.'''
-        self.ship_speed = 1.5
+        self.ship_speed = 1.2
         self.bullet_speed = 2.5
         self.alien_speed = 1.0
 
